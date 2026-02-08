873f646fc5c306ffc203edd55bc3e151c485a39c
Luisma Suarez <luisma.suarezzz@gmail.com>
2026-02-07 20:41:23 -0500

    fix(kanban): add SSR compatibility check for createPortal and replace PlusIcon with Tabler component
    Add server-side rendering check to prevent document access errors when rendering drag overlay portal, as document is undefined during SSR. Also replace custom PlusIcon SVG with imported IconPlus component from @tabler/icons-react for consistency with other icons in the kanban components.

--- components/kanban_example/KanbanBoard.tsx
+++ components/kanban_example/KanbanBoard.tsx
@@ -166,26 +166,28 @@ const KanbanBoard = () => {
                         ))}
                     </SortableContext>
 
-                    {createPortal(
-                        <DragOverlay>
-                            {activeColumn && (
-                                <ColumnContainer
-                                    column={activeColumn}
-                                    deleteColumn={deleteColumn}
-                                    updateColumn={updateColumn}
-                                    createTask={createTask}
-                                    tasks={tasks.filter(task => task.columnId === activeColumn.id)}
-                                    deleteTask={deleteTask}
-                                    updateTask={updateTask}
-                                    isOverlay={true}
-                                />
-                            )}
-                            {activeTask && (
-                                <TaskCard task={activeTask} deleteTask={deleteTask} updateTask={updateTask} />
-                            )}
-                        </DragOverlay>,
-                        document.body
-                    )}
+                    {typeof document !== 'undefined'
+                        ? createPortal(
+                            <DragOverlay>
+                                {activeColumn && (
+                                    <ColumnContainer
+                                        column={activeColumn}
+                                        deleteColumn={deleteColumn}
+                                        updateColumn={updateColumn}
+                                        createTask={createTask}
+                                        tasks={tasks.filter(task => task.columnId === activeColumn.id)}
+                                        deleteTask={deleteTask}
+                                        updateTask={updateTask}
+                                        isOverlay={true}
+                                    />
+                                )}
+                                {activeTask && (
+                                    <TaskCard task={activeTask} deleteTask={deleteTask} updateTask={updateTask} />
+                                )}
+                            </DragOverlay>,
+                            document.body
+                        )
+                        : null}
                 </DndContext>
             </div>
 
